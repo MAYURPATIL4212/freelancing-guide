089a7f47f655502197ea48270e55814272a07852
Patil Mayur <mayurgpatil2002@gmail.com>
2026-08-01 14:22:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Smooth Scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Fade Animation
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+document.querySelectorAll('.card').forEach(card => {
+    observer.observe(card);
+});
